4871ad6e444f92ded09d12b2b9a91749dec93f5a
Aleksei Morozov <a1exy.moroz@gmail.com>
2026-03-20 10:51:51 +0100

    Implement cafeteria function in index.js for counting ranges and update input data in input.txt. The new logic processes input to calculate the total number of items based on specified ranges, while maintaining file reading functionality.

--- 2025/5/index.js
+++ 2025/5/index.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
+const testData = `3-5
+10-14
+16-20
+12-18
 
-// console.log('Part 1: 13', printingDepartment(testData));
-// console.log('Part 2: 43', printingDepartment2(testData));
+1
+5
+8
+11
+17
+32`;
+
+const cafeteria = (input) => {
+    const lines = input.trim().split('\n');
+    const numbers = lines.map(line => line.split('-').map(Number));
+    return numbers.reduce((acc, curr) => acc + curr[1] - curr[0] + 1, 0);
+}
+
+console.log('Part 1: 13', cafeteria(testData));
+// console.log('Part 2: 43', cafeteria(testData));
 
 
 
@@ -12,7 +29,7 @@ const inputPath = path.join(__dirname, 'input.txt');
 try {
     const data = fs.readFileSync(inputPath, 'utf8');
     // console.log('Part 1: 13', printingDepartment(data));
-    console.log('Part 2: 13', printingDepartment2(data));
+    // console.log('Part 2: 13', cafeteria(data));
 } catch (err) {
     console.error('Error reading the input file:', err);
 }
