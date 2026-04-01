2d4bd1593fcc8d9e951692230c21e6c450480336
Aleksei Morozov <a1exy.moroz@gmail.com>
2026-04-01 14:56:48 +0200

    Add trashCompactorPart2 function in index.js to process input data for Part 2 of the Trash Compactor challenge. Update console logs to display results and enhance input handling with file reading functionality.

--- 2025/6. Trash Compactor/index.js
+++ 2025/6. Trash Compactor/index.js
@@ -32,13 +32,72 @@ const trashCompactor = (input) => {
    return sum;
 }
 
-console.log('Part 1: 4277556', trashCompactor(testData));
+const trashCompactorPart2 = (input) => {
+    const lines = input.trim().split('\n');
+    // console.log('lines', lines);
+    let numberLength = 0;
+
+    for (let number of lines[0].split(' ')) {
+        if (number.length > numberLength) {
+            numberLength = number.length;
+        }
+    }
+
+    console.log('numberLength', numberLength);
+    
+    for (let i = 0; i < lines.length; i++) {
+        const array = [];
+        for (let j = 0; j < lines[i].length; j += numberLength) {   
+            const number = lines[i].slice(j, j + numberLength);
+            array.push(number);
+            j++;
+        }
+        lines[i] = array;
+    }
+    const operators = lines.pop().map(char => char.trim());
+    // console.log('operators', operators);
+    // console.log('lines', lines);
+
+    // console.log('numberLength', numberLength);
+ 
+    const arrays = [];
+    for (let i = 0; i < lines[0].length; i++) {
+        const array = [];
+        for (let j = 0; j < lines.length; j++) {
+            const number = lines[j][i];
+            for (let k = 0; k < numberLength; k++) {
+                array[k] = array[k] ? array[k] + number[k] : number[k];
+            }
+        }
+        console.log('before array', array);
+        arrays.push(array.map(value => value.trim()).filter(value => value !== ''));
+        console.log('after array', arrays);
+    }
+    // console.log('arrays', arrays);
+ 
+    let sum = 0;
+ 
+    for (let i = 0; i < arrays.length; i++) {
+     const array = arrays[i];
+     const operator = operators[i];
+     if (operator === '+') {
+         sum += array.reduce((acc, num) => acc + Number(num), 0);
+     } else if (operator === '*') {
+         sum += array.reduce((acc, num) => acc * Number(num), 1);
+     }
+    }
+ 
+    return sum;
+ }
+
+// console.log('Part 1: 4277556', trashCompactor(testData));
+// console.log('Part 2: 3263827', trashCompactorPart2(testData));
 // Read the input file
 const inputPath = path.join(__dirname, 'input.txt');
 try {
     const data = fs.readFileSync(inputPath, 'utf8');
-    console.log('Part 1: 4277556', trashCompactor(data));
-    // console.log('Part 2: 13', trashCompactor(data));
+    // console.log('Part 1: 4277556', trashCompactor(data));
+    console.log('Part 2: 23159217', trashCompactorPart2(data));
 } catch (err) {
     console.error('Error reading the input file:', err);
 }
