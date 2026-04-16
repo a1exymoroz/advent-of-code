0d8ae0017e8bf2a4bf3879804530515fd552a627
Aleksei Morozov <a1exy.moroz@gmail.com>
2026-04-16 12:58:33 +0200

    Update laboratories function in index.js to refine grid processing logic. Adjust handling of 'S' and '^' characters, and enhance console output for clearer grid state representation. Correctly log results for both test and real input data.

--- 2025/7. Laboratories/index.js
+++ 2025/7. Laboratories/index.js
@@ -18,6 +18,8 @@ const testData = `.......S.......
 .^.^.^.^.^...^.
 ...............`;
 
+
+
 const laboratories = (input) => {
     const lines = input.split('\n');
     const grid = lines.map(line => line.split(''));
@@ -26,28 +28,45 @@ const laboratories = (input) => {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] === 'S') {
                 grid[i + 1][j] = '|';
-            } else if (grid[i][j] === '^') {
-                grid[i + 1][j + 1] = '|';
-                grid[i + 1][j - 1] = '|';
-            } 
-            else if (i + 1 < grid.length && grid[i][j] === '|' && grid[i][j] !== '^') {
-                grid[i + 1][j] = '|';
+            } else if (grid[i][j] === '^' && grid[i - 1][j] === '|') {
+                grid[i][j + 1] = '|';
+                grid[i][j - 1] = '|';
+                count++;
+            } else if (grid[i][j] === '.' && grid?.[i - 1]?.[j] === '|') {
+                grid[i][j] = '|';
             }
 
         }
     }
 
-    console.log(grid.map(line => line.join('')).join('\n'));
+    // console.log(grid.map(line => line.join('')).join('\n'));
     return count;
 }
 
-console.log('Part 1: 4277556', laboratories(testData));
+// console.log(`.......S.......
+// .......|.......
+// ......|^|......
+// ......|.|......
+// .....|^|^|.....
+// .....|.|.|.....
+// ....|^|^|^|....
+// ....|.|.|.|....
+// ...|^|^|||^|...
+// ...|.|.|||.|...
+// ..|^|^|||^|^|..
+// ..|.|.|||.|.|..
+// .|^|||^||.||^|.
+// .|.|||.||.||.|.
+// |^|^|^|^|^|||^|
+// |.|.|.|.|.|||.|`);
+
+console.log('Part 1: 21', laboratories(testData));
 // console.log('Part 2 (test): 3263827', laboratoriesPart2(testData));
 // Read the input file
 const inputPath = path.join(__dirname, 'input.txt');
 try {   
     const data = fs.readFileSync(inputPath, 'utf8');
-    // console.log('Part 1: 4277556', laboratories(data));
+    console.log('Part 1: 4277556', laboratories(data));
     // console.log('Part 2 (real):', laboratoriesPart2(data));
 } catch (err) {
     console.error('Error reading the input file:', err);
