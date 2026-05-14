25b036df8fbec941244dd60b81b6b6318bd55134
Aleksei Morozov <a1exy.moroz@gmail.com>
2026-05-14 15:34:23 +0200

    Add a new function `isGood2` to check if an array is good, along with example usage for validation. The function includes additional logic to handle edge cases and improve accuracy. Updated the file to ensure proper formatting with a newline at the end.

--- 2026/leetCode/array/Check if Array is Good.js
+++ 2026/leetCode/array/Check if Array is Good.js
@@ -45,3 +45,26 @@ var isGood = function(nums) {
 
     return nums[length] === length && nums[length - 1] === length
 };
+
+var isGood2 = function(nums) {
+    const n = nums.length;
+    const count = new Array(n).fill(0);
+    for (const a of nums) {
+        if (a >= n) {
+            return false;
+        }
+        if (a < n - 1 && count[a] > 0) {
+            return false;
+        }
+        if (a === n - 1 && count[a] > 1) {
+            return false;
+        }
+        count[a]++;
+    }
+    return true;
+}
+
+console.log(isGood([2, 1, 3]));
+console.log(isGood([1, 3, 3, 2]));
+console.log(isGood([1, 1]));
+console.log(isGood([3, 4, 4, 1, 2, 1]));
